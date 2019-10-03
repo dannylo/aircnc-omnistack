ccd322d11feebef38443a5df53ac8581c2d00bab
Dannylo Johnathan <dannylojohnathan@gmail.com>
2019-10-03 17:09:40 -0300

    adição do cors para permitir requisições externas

--- server-node/src/server.js
+++ server-node/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
@@ -12,6 +13,7 @@ mongoose.connect('mongodb+srv://aircnc-user:aircncuser@aircnc-upwvj.mongodb.net/
 });
 
 //plugin 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
