3eecbad310ba4194d90d2d4c14873a564f33485d
Dannylo Johnathan <dannylojohnathan@gmail.com>
2019-10-07 10:37:09 -0300

    Aplicação web ouvindo mensagens do socket para receber mensagens aceitação de requisições de visita.

--- react-web-app/frontend/src/pages/Dashboard/index.js
+++ react-web-app/frontend/src/pages/Dashboard/index.js
@@ -1,14 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import api from '../../services/api';
 import { Link } from 'react-router-dom';
+import socketio from 'socket.io-client';
+
 import './styles.css';
 export default function Dashboard(){
 
     const [spots, setSports] = useState([]);
+    const [requests, setRequests] = useState([]);
+
+    const user = localStorage.getItem('user');
+    //O useMemo está garantindo que a conexão de socket só será refeita, caso o user seja modificado. Caso contrário, a conexão permanecerá memorizada.
+    const socket = useMemo(() => socketio('http://localhost:3333', {
+        query: { user },
+    }), [user]);
     //useEffect é uma função.
     //função passada no primeiro parâmetro é executada sempre que as variáveis
     //listadas no array de dependencias(segundo parametro) sofrerem alguma modificação.
     // o array vazio diz que a função no userEffect só executará uma única vez.
+   
+   useEffect(() => {
+        //a notação abaixo ...requests, ajuda ao comando não sobrescrever os dados do array, e sim coloca-las no final.
+        socket.on('booking_request', data =>{
+            setRequests([... requests, data]);
+        })
+   }, [requests]);
+   
     useEffect(() => {
         async function loadSpots(){
             const user_id = localStorage.getItem('user');
@@ -22,8 +39,21 @@ export default function Dashboard(){
         loadSpots();
     }, []);
 
+
+
     return (
         <>
+            <ul className="notifications" >
+                {requests.map(request => (
+                    <li key={request.id}>
+                        <p>
+                            <strong>{request.user.email}</strong> está solicitando uma reserva em <strong>{request.spot.company}</strong> para a data <strong>{request.date}</strong>.
+                        </p>
+                        <button className="accept">Aceitar</button>
+                        <button className="reject">Rejeitar</button>
+                    </li>
+                ))}
+            </ul>
             <ul className="spot-list">
                 {spots.map(spot => (
                     <li key={spot._id}>
