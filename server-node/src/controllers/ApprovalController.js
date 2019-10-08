3704ff2fd92b83852f97acbf6095b0e845f61980
Dannylo Johnathan <dannylojohnathan@gmail.com>
2019-10-08 08:49:02 -0300

    Definição de controladores para aprovação e reprovação das reservas.

---
+++ server-node/src/controllers/ApprovalController.js
@@ -0,0 +1,18 @@
+const Booking = require('../models/Booking');
+
+module.exports ={
+    async store(req, res) {
+        const { booking_id } = req.params;
+        const booking = await Booking.findById(booking_id).populate('spot');
+        booking.approved = true;
+        await booking.save();
+
+        //enviando uma mensagem para o usuário interessado na reserva
+        const userBookingSocket = req.connectedUsers[booking.user];
+        if(userBookingSocket){
+            req.io.to(userBookingSocket).emit('booking_response', booking);
+        }
+        
+        return res.json(booking);
+    }
+}
--- server-node/src/models/Spot.js
+++ server-node/src/models/Spot.js
@@ -21,7 +21,7 @@ const SpotSchema = new mongoose.Schema({
 //define um atributo virtual para o modelo, criando o campo responsável por direcionar
 //o thumbnail para a pasta de arquivos no servidor.
 SpotSchema.virtual('thumbnail_url').get(function(){
-    return `http://localhost:3333/files/${this.thumbnail}`;
+    return `http://10.107.1.24:3333/files/${this.thumbnail}`;
 });
 
 module.exports = mongoose.model('Spot', SpotSchema);
